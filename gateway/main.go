1030a9d2abfd748f85c53cc7c5d371ac0b5a316c
aligator <aligator@suncraft-server.de>
2020-10-19 00:27:19 +0200

    Add gateway with graphql

--- gateway/main.go
+++ gateway/main.go
@@ -1,22 +1,61 @@
-// file main.go is just for testing the microservices without full implementation while developing.
+//go:generate go run github.com/99designs/gqlgen generate
 package main
 
 import (
 	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
 
-	"google.golang.org/grpc"
-
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/jdc-lab/daily-shit/common"
+	"github.com/jdc-lab/daily-shit/gateway/graph"
+	"github.com/jdc-lab/daily-shit/gateway/graph/generated"
 	pb "github.com/jdc-lab/daily-shit/user-service/proto/user"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	// Todo
+const defaultPort = "8080"
 
+func main() {
+	service := common.GetRandomServiceWithConsul("user-service")
+	if service == nil {
+		panic("no user-service found")
+	}
+
+	host := net.JoinHostPort(service.Service.Address, strconv.Itoa(service.Service.Port))
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(":8100", grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(host, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() { _ = conn.Close() }()
-	_ = pb.NewUserServiceClient(conn)
+	userService := pb.NewUserServiceClient(conn)
+
+	// ToDo: register also with consul
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
+		Resolvers: &graph.Resolver{
+			UserService: userService,
+		},
+		Directives: graph.Directives(),
+	}))
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(Authenticator(userService))
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/v1"))
+	r.Handle("/v1", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, r))
 }
